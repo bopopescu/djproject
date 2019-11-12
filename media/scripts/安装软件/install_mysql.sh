a5e32dc652a8b7129609c5e612e8daf69cfda58e
huhaiqng <hu158168>
2019-11-12 09:05:03 +0800

    1321385590@qq.com

--- media/scripts/安装软件/install_mysql.sh
+++ media/scripts/安装软件/install_mysql.sh
@@ -1,7 +1,1 @@
 #!/bin/bash
-echo "---------------------------- 将静态文件发布到 188.188.1.142 ----------------------------"
-ssh tomcat@188.188.1.142 "sh /home/tomcat/scripts/deploy_baipao_front.sh"
-echo "---------------------------- 将静态文件发布到 188.188.1.143 ----------------------------"
-ssh tomcat@188.188.1.143 "sh /home/tomcat/scripts/deploy_baipao_front.sh"
-# echo "---------------------------- 将静态文件传到 188.188.1.133 ----------------------------"
-# ssh devuser@188.188.1.133 "sh /home/devuser/scripts/deploy_baipao_front.sh"
---
+++ static/js/tasks.js
@@ -0,0 +1,45 @@
+$(function () {
+    $('#a_task').addClass('active')
+    $('#a_exec_task').addClass('active')
+    $('#ul_task').addClass('in')
+
+    $("#exec_tasks").click(function () {
+        var hosts = document.getElementsByName("host")
+        ips =[]
+        for (i=0; i<hosts.length; i++){
+            if (hosts[i].checked){
+                ip = hosts[i].value;
+                ips.push(ip)
+            }
+        }
+
+        var tasks = document.getElementsByName("task")
+        tks =[]
+        for (i=0; i<tasks.length; i++){
+            if (tasks[i].checked){
+                t_value = tasks[i].value;
+                tks.push(t_value)
+            }
+        }
+
+        data = {'ips':ips,'tks':tks}
+
+        $('button').prop('disabled', true)
+        $('#messagecontainer').empty()
+
+        var socket = new WebSocket("ws://" + window.location.host + "/common/exec_tasks/")
+        socket.onopen = function () {
+            socket.send(JSON.stringify(data))
+        };
+        socket.onmessage = function (e) {
+            if (e.data){
+                if(e.data == 'over'){
+                    $('button').prop('disabled', false)
+                }
+                else{
+                    $('#messagecontainer').append(e.data+'<br/>');
+                }
+            }
+        };
+    })
+})
